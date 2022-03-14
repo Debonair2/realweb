8c4841d961a800f259dd13e98f3902acae13e453
Debonair2 <dareblessing12@gmail.com>
2022-03-14 18:17:17 +0100

    Initial Commit

---
+++ main.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/elements.html")
+def elements():
+    return render_template("elements.html")
+
+@app.route("/index.html")
+def index():
+    return render_template("index.html")
+
+@app.route("/generic.html")
+def generic():
+    return render_template("generic.html")
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
